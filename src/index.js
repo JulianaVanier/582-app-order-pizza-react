a90d38df39693e2caeb6564fcf9f9af2136fd859
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-09-17 18:59:16 -0400

    started creation for store

--- src/index.js
+++ src/index.js
@@ -7,6 +7,12 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import {configureStore} from "@reduxjs/toolkit";
+import {Provider} from "react-redux";
+
+const store = configureStore({
+  reducer: {},
+});
 
 const router = createBrowserRouter([
   {
@@ -22,7 +28,9 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider store={store}>
     <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
 
