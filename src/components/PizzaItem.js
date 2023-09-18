425c1671bbd5200db0a4e9bc5e7955d4ff3c3138
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-09-18 15:44:03 -0400

    created func btn for pizza customize/add to cart

--- src/components/PizzaItem.js
+++ src/components/PizzaItem.js
@@ -8,17 +8,25 @@ import React from 'react'
 function PizzaItem(props) {
   console.log("AQUI props", props)
 
+  //function to modify button text
+  function customButton(){
+    if(props.obj.custom === true){
+      return <span>Customize</span>
+    }else{
+      return <span>Add to Cart</span>
+    }
+  }
+
   // const pizza = useSelector((state) => state.pizza.pizzas)
   return (
   <>
-
         <div className='col-md-3'>
           <div className="card">
             <img className="card-img-top" src={props.obj.imageReact} alt="Card image cap"/>
             <div className="card-body">
               <h5 className="card-title">{props.obj.title}</h5>
               <p className="card-text">{props.obj.description}</p>
-              <a href="#" className="btn btn-danger">Add to Cart</a>
+              <a href="#" className="btn btn-danger">{customButton()}</a>
             </div>
           </div>
         </div>
