2af9098ca246dacc19b8ac68e59e395d076cbc33
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-09-29 09:31:47 -0400

    working in pizza custom to save ingredients

--- src/components/CustomPizzaView.js
+++ src/components/CustomPizzaView.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import CustomPizza from './CustomPizza'
-import { Container } from 'react-bootstrap';
+import React from "react";
+import CustomPizza from "./CustomPizza";
+import { Container } from "react-bootstrap";
 
 function CustomPizzaView() {
   return (
     <>
-    <Container >
-    <h1>Custom Pizza</h1>
-    <CustomPizza/>
-    </Container>
+      <Container>
+        <h1>Custom Pizza</h1>
+        <CustomPizza />
+      </Container>
     </>
-  )
+  );
 }
 
-export default CustomPizzaView
+export default CustomPizzaView;
--- src/components/IngredientItem.js
+++ src/components/IngredientItem.js
@@ -1,105 +1,126 @@
-import { render } from '@testing-library/react';
-import React, { useEffect } from 'react'
-import { Button, Card } from 'react-bootstrap'
+import { render } from "@testing-library/react";
+import React, { useEffect } from "react";
+import { Button, Card } from "react-bootstrap";
 // import { useSelector } from 'react-redux'
 // import logo from '../assests/images/broccoli-pizza.webp';
-import Col from 'react-bootstrap/Col';
-import { useSelector } from 'react-redux';
-
-
+import Col from "react-bootstrap/Col";
+import { useSelector } from "react-redux";
 
 function IngredientItem(props) {
+  const pizzaToCustomize = useSelector((state) => state.pizza.pizzaToCustomize);
+  // const dispatch = useDispatch();
+  const ingredient = props.obj;
+  const pizzaId = props.pizzaId;
+  const [displayIngTop, setDisplayIngTop] = React.useState(false);
 
-const pizzaToCustomize = useSelector((state) => state.pizza.pizzaToCustomize);
-// const dispatch = useDispatch();
-const ingredient = props.obj;
-const pizzaId = props.pizzaId;
-const [displayIngTop, setDisplayIngTop] = React.useState(false);
+  // const [showIngredientsTop, setShowIngredientsTop] = React.useState(false);
+  // const displayIngredientsTop = () => setShowIngredientsTop(!showIngredientsTop)
 
-// const [showIngredientsTop, setShowIngredientsTop] = React.useState(false);
-// const displayIngredientsTop = () => setShowIngredientsTop(!showIngredientsTop)
+  console.log("pizzaaaaaaaaa22222222222222222", pizzaId);
+
+  function toggleIngredient(ingredient, pizzaId, pizzaToCustomize) {
+    // console.log("deuuuuuuuuuuuuu ing", ingredient);
+    // console.log("deuuuuuuuuuuuuu idp", pizzaId);
+    // console.log("deuuuuuuuuuuuuu cust", pizzaToCustomize);
+    var pizzaCustomWithIngredient = null;
 
 
-console.log("pizzaaaaaaaaa22222222222222222", pizzaId)
+ 
 
-function toggleIngredient(ingredient, pizzaId, pizzaToCustomize){
-
-  console.log("deuuuuuuuuuuuuu ing", ingredient);
-  console.log("deuuuuuuuuuuuuu idp", pizzaId);
-  console.log("deuuuuuuuuuuuuu cust", pizzaToCustomize);
-  var pizzaCustomWithIngredient = null;
-  console.log("pizzaToCustomize", pizzaToCustomize);
-  console.log("pizzaId", pizzaId);
-  console.log("ingredient", ingredient);
-  for (let i = 0; i < pizzaToCustomize.length; i++) {
-    console.log("pizzaToCustomize[i].id", pizzaToCustomize[i].id);
-    if (pizzaToCustomize[i].id == pizzaId) {
-      pizzaCustomWithIngredient = pizzaToCustomize[i];
+    // console.log("pizzaToCustomize", pizzaToCustomize);
+    // console.log("pizzaId", pizzaId);
+    // console.log("ingredient", ingredient);
+    for (let i = 0; i < pizzaToCustomize.length; i++) {
+      // console.log("pizzaToCustomize[i].id", pizzaToCustomize[i].id);
+      if (pizzaToCustomize[i].id == pizzaId) {
+        pizzaCustomWithIngredient = pizzaToCustomize[i];
+      }
     }
-  }
 
-  for (let i = 0; i < pizzaCustomWithIngredient.ingredient.length; i++) {
-    console.log("lengthhhhhhhhhhh", pizzaCustomWithIngredient.ingredient.length);
-    if (pizzaCustomWithIngredient.ingredient[i]._id === ingredient._id) {
+    // condition if ingredient already exist in pizzaCustomWithIngredient
+    for (let i = 0; i < pizzaCustomWithIngredient.ingredient.length; i++) {
+      // console.log(
+      //   "lengthhhhhhhhhhh",
+      //   pizzaCustomWithIngredient.ingredient.length
+      // );
+      console.log("ID INGREDIENT", ingredient._id);
+      console.log("ID INGREDIENT PIZZA CUSTOM", pizzaCustomWithIngredient);
+      if (pizzaCustomWithIngredient.ingredient[i]._id === ingredient._id) {
+        console.log("ingredient exist");
+        const updatedPizza = { ...pizzaCustomWithIngredient };
 
-      const updatedPizza = { ...pizzaCustomWithIngredient };
-
-      updatedPizza.ingredient =
-        pizzaCustomWithIngredient.ingredient.filter(
+        updatedPizza.ingredient = updatedPizza.ingredient.filter(
           (ing) => ing._id !== ingredient._id
         );
         updatedPizza.sizeSelectedPrice -= ingredient.price;
         pizzaCustomWithIngredient = updatedPizza;
-      return;
+        return;
+      }
     }
+
+    // toggle to display ingredient on top of pizza
+    setDisplayIngTop((prev) => !prev);
+
+    // for (let i = 0; i < pizzaCustomWithIngredient.ingredient.length; i++) {
+    //   if(pizzaCustomWithIngredient.ingredient[i]._id == ingredient._id){
+    //     return(
+    //       <img src={ingredient.imageCustomReact} alt={ingredient.title} />
+    //     )
+    //   }
+    // }
+
+    const updatedPizza = { ...pizzaCustomWithIngredient };
+    updatedPizza.ingredient = [...updatedPizza.ingredient, ingredient];
+    // updatedPizza.sizeSelectedPrice += ingredient.price;
+    pizzaCustomWithIngredient = updatedPizza;
+    console.log("pizzaCustomWithIngredient FINAL", pizzaCustomWithIngredient);
+
+    // const updatedPizza = { ...pizzaCustomWithIngredient };
+    // updatedPizza.ingredient = [...ingredient];
+    // console.log("updatedPizza", updatedPizza);
+    // updatedPizza.sizeSelectedPrice =
+    //   pizzaCustomWithIngredient.sizeSelectedPrice;
+    // pizzaCustomWithIngredient = updatedPizza;
+    // // pizzaCustomWithIngredient.ingredient.push(ingredient);
+    // console.log("pizzaCustomWithIngredient", pizzaCustomWithIngredient);
+    //  pizzaCustomWithIngredient.sizeSelectedPrice += ingredient.price;
   }
-  setDisplayIngTop((prev) => !prev);
-
-  // for (let i = 0; i < pizzaCustomWithIngredient.ingredient.length; i++) {
-  //   if(pizzaCustomWithIngredient.ingredient[i]._id == ingredient._id){
-  //     return(
-  //       <img src={ingredient.imageCustomReact} alt={ingredient.title} />
-  //     )
-  //   }
-  // }
-
-  
-  const updatedPizza = { ...pizzaCustomWithIngredient };
-  updatedPizza.ingredient = [...pizzaCustomWithIngredient.ingredient];
-  updatedPizza.sizeSelectedPrice = pizzaCustomWithIngredient.sizeSelectedPrice;
-  pizzaCustomWithIngredient = updatedPizza;
-  // pizzaCustomWithIngredient.ingredient.push(ingredient);
-  console.log("pizzaCustomWithIngredient", pizzaCustomWithIngredient);
-  //  pizzaCustomWithIngredient.sizeSelectedPrice += ingredient.price;
-}
-
-
 
   // const pizza = useSelector((state) => state.pizza.pizzas)
   return (
     <>
-    {/* <h1>{props}</h1> */}
-    <Col className='ingredients'>
-      <Card style={{ width: '10rem' }} onClick={() => toggleIngredient(ingredient, pizzaId, pizzaToCustomize)}>
-      <Card.Title>{ingredient.title}</Card.Title>
-        <Card.Img variant="top" src={ingredient.imageReact} alt={ingredient.title} />
-        <Card.Body>
+      {/* <h1>{props}</h1> */}
+      <Col className="ingredients">
+        <Card
+          style={{ width: "10rem" }}
+          onClick={() =>
+            toggleIngredient(ingredient, pizzaId, pizzaToCustomize)
+          }
+        >
+          <Card.Title>{ingredient.title}</Card.Title>
+          <Card.Img
+            variant="top"
+            src={ingredient.imageReact}
+            alt={ingredient.title}
+          />
+          <Card.Body>
             <Card.Text>
-            {ingredient.description}
-            {ingredient.price}
+              {ingredient.description}
+              {ingredient.price}
             </Card.Text>
             {/* <Button className='btn-add-chose-size' variant="danger" onClick={() => toggleIngredient(ingredient, pizzaId, pizzaToCustomize)}>
             Add
             </Button> */}
-                <div>
-      {displayIngTop && <img src={ingredient.imageCustomReact} alt={ingredient.title} />}
-    </div>
-        </Card.Body>
-      </Card>
-    </Col>
-
+            <div className="image-top-pizzaCustom">
+              {displayIngTop && (
+                <img src={ingredient.imageCustomReact} alt={ingredient.title} />
+              )}
+            </div>
+          </Card.Body>
+        </Card>
+      </Col>
     </>
-  )
+  );
 }
 
-export default IngredientItem
+export default IngredientItem;
