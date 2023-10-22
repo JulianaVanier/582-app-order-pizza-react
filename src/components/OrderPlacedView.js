71f51769665ef056f2485d8a4d398853ef5079c6
Juliana <2295471@edu.vaniercollege.qc.ca>
2023-10-22 19:03:00 -0400

    fixed styles

--- src/components/OrderPlacedView.js
+++ src/components/OrderPlacedView.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
+import TopBar from './TopBar';
 
 function OrderPlacedView() {
     const location = useLocation();
     const orderNumber = new URLSearchParams(location.search).get('orderNumber');
 
   return (
+
     <Container>
+      <TopBar />
         <h1>Order Placed</h1>
         <h2>Your order number is: {orderNumber}</h2>
+        <div className='img-order'>
         <img src="/assets/image/delivery.gif" alt="pizza gif" />
+        </div>
     </Container>
   )
 }
